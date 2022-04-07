1cfcaaee365c6f8d08306394f8810cd6ec63242e
Khristian Mangubat <kiritian2@gmail.com>
2022-04-07 08:28:38 +0800

    Added firing and deleted unnecessary files

---
+++ Bullet.gd
@@ -0,0 +1,6 @@
+extends RigidBody2D
+
+
+
+func _on_Bullet_body_entered(body):
+	queue_free()
--- Tank.gd
+++ Tank.gd
@@ -1,34 +1,32 @@
 extends KinematicBody2D
 
 var movement = Vector2();
-var movespeed = 200;
+export var movespeed = 250;
+var bullet = preload("res://Bullet.tscn")
+export var bullet_speed = 1000;
 
 func _physics_process(_delta):
+	
+	#For firing
+	if(Input.is_action_just_pressed("fire")):
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = $Sprite/Bullet_Point.get_global_position()
+		bullet_instance.rotation_degrees = rotation_degrees + 90
+		print(bullet_instance.rotation_degrees)
+		print(rotation_degrees)
+		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed,0).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)
 	
 	# Movements of the tank
 	#For Left and right
 	if(movement.y == 0):
-		movement.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * movespeed;
-		
-		#Rotating the tank Left And Right
-		if(movement.x == 0):
-			pass
-		if(Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right")):
-			$Sprite.set_rotation(-1.5708)
-		elif(Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left")):
-			$Sprite.set_rotation(1.5708)
-	
+		movement.x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * movespeed;
 	#For Up and Down
 	if(movement.x == 0):
-		movement.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) * movespeed;
-		
-		#Rotating the tank Up and Down
-		if(movement.y == 0):
-			pass
-		if(Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_down")):
-			$Sprite.set_rotation(0)
-		elif(Input.is_action_pressed("ui_down") && !Input.is_action_pressed("ui_up")):
-			$Sprite.set_rotation(-3.14159)
+		movement.y = (Input.get_action_strength("move_down") - Input.get_action_strength("move_up")) * movespeed;
+
+	if(movement != Vector2.ZERO):
+		set_rotation(movement.angle())
 	
 	
 	movement = move_and_slide(movement)
